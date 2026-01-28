c56a10383722e04773519f59f7a79756725105e8
Sahil Arora <sahil.arora@accenture.com>
2026-01-28 13:24:30 -0500

    Updated Apex Classes and added new class for agent Actions

--- AgentforceChallenge1/force-app/main/default/lwc/accountRecord/accountRecord.js
+++ AgentforceChallenge1/force-app/main/default/lwc/accountRecord/accountRecord.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class AccountRecord extends LightningElement {}
+export default class AccountListDisplay extends LightningElement {
+    @api accounts; // receives List<AccountOutputWrapper> from Apex
+
+    // True if there are accounts to display
+    get hasAccounts() {
+        return this.accounts && this.accounts.length > 0;
+    }
+
+    // No mapping needed, wrapper already matches template
+    get formattedAccounts() {
+        return this.accounts;
+    }
+}
