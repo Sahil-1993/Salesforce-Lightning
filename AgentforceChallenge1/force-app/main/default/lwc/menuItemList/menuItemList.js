bc115d4047be666da7a37faa749e220512693699
Sahil Arora <sahil.arora@accenture.com>
2026-02-08 19:28:37 -0500

    updated code

--- AgentforceChallenge1/force-app/main/default/lwc/menuItemList/menuItemList.js
+++ AgentforceChallenge1/force-app/main/default/lwc/menuItemList/menuItemList.js
@@ -1,15 +1,14 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class MenuItemList extends LightningElement {
-    @api items; // receives List<MenuItemOutput> from Apex
+    @api value; 
+    // value = MenuItemWrapperOutput from Lightning Type
 
-    // True if there are menu items to display
     get hasMenuItems() {
-        return this.items && this.items.length > 0;
+        return this.value?.menuItems?.length > 0;
     }
 
-    // No mapping needed, wrapper already matches template
     get formattedMenuItems() {
-        return this.items;
+        return this.value?.menuItems || [];
     }
 }
